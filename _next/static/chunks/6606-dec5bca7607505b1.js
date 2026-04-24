a4a5a058f46e817186583947cd1894887778eb54
contato22 <contato22@users.noreply.github.com>
2026-04-24 18:08:53 +0000

    deploy: c01c717cd34336444e125fee1100d71aeb90973d

---
+++ _next/static/chunks/6606-dec5bca7607505b1.js
@@ -0,0 +1,11 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6606],{46014:function(t,n,a){a.d(n,{dr:function(){return u},en:function(){return e},ge:function(){return i},k4:function(){return o},pU:function(){return r},uZ:function(){return c}}),a(7293),a(75683);let o=["Lead Captado","Qualifica\xe7\xe3o","Briefing Inicial","Proposta Enviada","Negocia\xe7\xe3o","Fechado Ganho","Fechado Perdido"],i=["V\xeddeo Publicit\xe1rio","Filme Institucional","Evento / Live","Conte\xfado Digital","Fotografia","Motion / Anima\xe7\xe3o","Outro"],e=["Indica\xe7\xe3o","Instagram","LinkedIn","WhatsApp","Site","Evento","Outro"],r=["Novo","Qualificando","Convertido","Perdido","Nurturing"],c=["Em Elabora\xe7\xe3o","Enviada","Aprovada","Rejeitada","Em Revis\xe3o"],u=["Baixo","M\xe9dio","Alto"]},90775:function(t,n,a){a.d(n,{_:function(){return o}});async function o(t){try{let n=await fetch("".concat("/awq","/data/caza-crm-").concat(t,".json"));if(n.ok)try{let t=await n.json();return Array.isArray(t)?t:[]}catch(t){}}catch(t){}return[]}},24341:function(t,n,a){a.d(n,{Z:function(){return o}});/**
+ * @license lucide-react v0.435.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let o=(0,a(78030).Z)("CloudOff",[["path",{d:"m2 2 20 20",key:"1ooewy"}],["path",{d:"M5.782 5.782A7 7 0 0 0 9 19h8.5a4.5 4.5 0 0 0 1.307-.193",key:"yfwify"}],["path",{d:"M21.532 16.5A4.5 4.5 0 0 0 17.5 10h-1.79A7.008 7.008 0 0 0 10 5.07",key:"jlfiyv"}]])},97589:function(t,n,a){a.d(n,{Z:function(){return o}});/**
+ * @license lucide-react v0.435.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let o=(0,a(78030).Z)("Database",[["ellipse",{cx:"12",cy:"5",rx:"9",ry:"3",key:"msslwz"}],["path",{d:"M3 5V19A9 3 0 0 0 21 19V5",key:"1wlel7"}],["path",{d:"M3 12A9 3 0 0 0 21 12",key:"mv7ke4"}]])}}]);
